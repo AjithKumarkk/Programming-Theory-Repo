b4e7d4612ace2408af8e6705c61908781a86d78e
AjithKumarkk <38382915+AjithKumarkk@users.noreply.github.com>
2023-10-22 20:27:13 +0530

    intermediate commit
    working start button

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build.Content;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
